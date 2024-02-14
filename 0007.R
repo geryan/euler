76fe5fd30d7631eeb3571c27a5d1c3008bdc1f66
geryan <geryan@users.noreply.github.com>
2024-02-15 10:49:12 +1100

    complete 0007

--- 0007.R
+++ 0007.R
@@ -1,3 +1,10 @@
-#By listing the first six prime numbers: 2, 3, 5, 7, 11 and 13, we can see that the 6th prime is 13.
+# By listing the first six prime numbers: 2, 3, 5, 7, 11 and 13, we can see that the 6th prime is 13.
 
-#What is the 10001st prime number?
+# What is the 10001st prime number?
+
+library(primes)
+
+z <- generate_n_primes(10001)
+z[10001]
+
+# this feels like cheating....
